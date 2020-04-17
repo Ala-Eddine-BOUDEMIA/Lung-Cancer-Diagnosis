fb4a308efe8472b002826ec105e8c3fb5adeac40
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-04-17 15:58:16 +0100

    Modified patches generation

--- Run.py
+++ Run.py
@@ -1,21 +1,26 @@
 import Split
-import Patches
 import Make_CSV_files
-#####################
+import Generate_224_Patches
+###########################
 
-def Main():
-	ans = 0
-	while ans != 4:
-
-		print("1-Split the data")
-		print("2-Create csv files containing training, validating and testing labels")
-		print("3-Create patches from the original WSI (SVS to JPEG)")
-		print("4-Quit")
-		ans = input("Enter your choice: ")
+def Main(ans):
 
 	if ans == 1:
 		Split.split()
 	elif ans == 2:
 		Make_CSV_files.Make_csv_labels_files()
 	elif ans == 3:
-		Patches.read_patch()
+		Generate_224_Patches.generate_patches()
+
+if __name__ == '__main__':
+	
+	ans = 0
+	while ans != 4:
+
+		print("1- Split the data")
+		print("2- Create csv files containing training, validating and testing labels")
+		print("3- Create patches of size 224*224 from the original WSIs")
+		print("4-Quit")
+		ans = input("Enter your choice: ")
+
+	Main(ans)
