3c0800a1d86203bd70df0cb84e778ea2560432d8
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-04-22 14:35:28 +0100

    Modified code structure

--- Model.py
+++ Model.py
@@ -3,8 +3,6 @@ import copy
 import time                 
 import Config                                                            
 #############
-from matplotlib import pyplt as plt
-###################################
 import torch                                
 import torchvision                          
 from torch import nn                        
@@ -15,7 +13,9 @@ from torchvision import datasets
 from torchvision import transforms                      
 from torch.optim import lr_scheduler              
 from torch.optim.lr_scheduler import ExponentialLR      
-##################################################   
+##################################################
+from matplotlib import pyplot as plt
+####################################  
 
 def load_data(path, batch_size, shuffle):
 
@@ -24,7 +24,7 @@ def load_data(path, batch_size, shuffle):
 	images_dataset = datasets.ImageFolder(root = str(path), transform = data_transforms) 
 	dataloaders = torch.utils.data.DataLoader(dataset = images_dataset, batch_size = batch_size, shuffle = shuffle, num_workers = 8)
 
-	return dataloaders
+	return dataloaders, images_dataset
 
 def create_model(device):
                                                 
@@ -36,103 +36,107 @@ def create_model(device):
 
     return model
 
-def optimizer(model, learning_rate = Config.args.learning_rate, 
-	learning_rate_decay = Config.args.learning_rate_decay, 
-	weight_decay = Config.args.weight_decay):
-
-	opt = optim.Adam(params = model.parameters(), lr = learning_rate, weight_decay = weight_decay)
-	scheduler = lr_scheduler.ExponentialLR(optimizer = opt, gamma = learning_rate_decay)
+def get_current_lr(opt):
 
 	current_lr = None
 	for group in opt.param_groups:
 		current_lr = group["lr"]
 
-	return opt, scheduler, current_lr
+	return current_lr
 
 def metrics_batch(output, target):
 	
 	predicted = torch.argmax(output, dim = 1, keepdim = True)
 	corrects = predicted.eq(target.view_as(predicted)).sum().item()
 
-	return corrects 
-
-def loss_batch(output, target, optimizer, loss_function = nn.CrossEntropyLoss()):
-
- 	loss = loss_function(output, target) 
- 	metric_b = metrics_batch(output, target)
-
- 	if optimizer is None:
- 		optimizer.zero_grad()
- 		loss.backward()
- 		optimizer.step()
-
- 	return loss.item(), metric_b   
-
-def loss_epoch(model, device, loader, optimizer, sanity_check):
-
-	running_loss = 0.0
-	runing_metric = 0.0
-	
-	for i, (inputs, labels) in enumerate(loader):
-
-		inputs = inputs.to(device)
-		labels = labels.to(device)
-
-		outputs = model(inputs)
-		loss_b, metric_b = loss_batch(outputs, labels, optimizer)
-		running_loss += loss_b
-
-		if metric_b is not None:
-			runing_metric += metric_b
-
-		if sanity_check is True:
-			break
-
-	len_data = len(loader)
-	loss = running_loss / float(len_data)
-	metric = runing_metric / float(len_data)
-
-	return loss, metric
+	return corrects    
 
 def Train_Val(num_epochs = Config.args.num_epochs, batch_size = Config.args.batch_size, 
-	path2weights = Config.args.Path2Weights, Train_Patches_path = Config.args.Train_Patches, 
-	Validation_Patches_path = Config.args.Validation_Patches, sanity_check = Config.args.Sanity_Check):
+	weight_decay = Config.args.weight_decay, path2weights = Config.args.Path2Weights, 
+	learning_rate = Config.args.learning_rate, learning_rate_decay = Config.args.learning_rate_decay, 
+	Train_Patches_path = Config.args.Train_Patches, Validation_Patches_path = Config.args.Validation_Patches, 
+	sanity_check = Config.args.Sanity_Check, loss_function = nn.CrossEntropyLoss()):
 
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-	train_loader = load_data(path = Train_Patches_path, batch_size = batch_size, shuffle = True)
-	val_loader = load_data(path = Validation_Patches_path, batch_size = batch_size, shuffle = False)
+	train_loader, train_set = load_data(path = Train_Patches_path, batch_size = batch_size, shuffle = True)
+	val_loader, val_set = load_data(path = Validation_Patches_path, batch_size = batch_size, shuffle = False)
 
 	model = create_model(device)
 	best_model = copy.deepcopy(model.state_dict())
 	best_loss = float("inf")
 
-	opt, scheduler, current_lr = optimizer(model)
+	opt = optim.Adam(params = model.parameters(), lr = learning_rate, weight_decay = weight_decay)
+	scheduler = lr_scheduler.ExponentialLR(optimizer = opt, gamma = learning_rate_decay)
 
 	loss_history = {"train": [], "val": []}
 	metric_history = {"train": [], "val": []}
 
-
 	for epoch in range(num_epochs):
 
+		current_lr = get_current_lr(opt)
 		print('Epoch {}/{}, current lr={}'.format(epoch + 1, num_epochs, current_lr))
-		
+
 		model.train()
-		train_loss, train_metric = loss_epoch(model, device, train_loader, opt, sanity_check = sanity_check)
+		train_running_loss = 0.0
+		train_runing_metric = 0.0
+
+		for i, (inputs, labels) in enumerate(train_loader):
+
+			train_inputs = inputs.to(device)
+			train_labels = labels.to(device)
+
+			train_outputs = model(train_inputs)
+			train_loss_b = loss_function(train_outputs, train_labels) 
+			train_metric_b = metrics_batch(train_outputs, train_labels)
+
+			opt.zero_grad()
+			train_loss_b.backward()
+			opt.step()
+			train_running_loss += train_loss_b
+
+			if train_metric_b is not None:
+				train_runing_metric += train_metric_b
+
+		train_len_data = len(train_set)
+		train_loss = train_running_loss / float(train_len_data)
+		train_metric = train_runing_metric / float(train_len_data)
 		loss_history["train"].append(train_loss)
 		metric_history["train"].append(train_metric)
 
 		model.eval()
-		with torch.no_grad():
-			val_loss, val_metric = loss_epoch(model, device, val_loader, opt, sanity_check = sanity_check)
-			loss_history["val"].append(val_loss)
-			metric_history["val"].append(val_metric)
+		val_running_loss = 0.0
+		val_runing_metric = 0.0
 
-			if val_loss < best_loss:
-				best_loss = val_loss
-				best_model = copy.deepcopy(model.state_dict())
-				torch.save(model.state_dict(), path2weights) 
-				print("Copied best model weights")
+		for i, (inputs, labels) in enumerate(val_loader):
+
+			val_inputs = inputs.to(device)
+			val_labels = labels.to(device)
+
+			with torch.no_grad():
+				val_outputs = model(val_inputs)
+				val_loss_b = loss_function(val_outputs, val_labels) 
+				val_metric_b = metrics_batch(val_outputs, val_labels)
+
+				if val_loss_b < best_loss:
+					best_loss = val_loss_b
+					best_model = copy.deepcopy(model.state_dict())
+					torch.save(model.state_dict(), path2weights) 
+					print("Copied best model weights")
+
+			val_running_loss += val_loss_b
+
+			if val_metric_b is not None:
+				val_runing_metric += val_metric_b
+
+			if sanity_check is True:
+				break
+
+		val_len_data = len(val_set)
+		val_loss = val_running_loss / float(val_len_data)
+		val_metric = val_runing_metric / float(val_len_data)
+		loss_history["val"].append(val_loss)
+		metric_history["val"].append(val_metric)
 
 		scheduler.step()
 
