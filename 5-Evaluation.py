2e47e83f8f895fb1c00819c5a9d8cbf5ddcba6af
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-05-07 00:05:46 +0100

    predict on wsi level

--- 5-Evaluation.py
+++ 5-Evaluation.py
@@ -1,29 +1,47 @@
 import Utils
 import Config 
 #############
+import csv
+import operator
+###############
 from itertools import islice
 ############################
 
-def get_prediction(Tets_Patches_path = Config.args.Test_Patches):
+def get_prediction(Train_folder = Config.args.Train_folder, output_folder = Config.args.Predictions):
 
-	path_list, csv_paths = Utils.parse_dir(Tets_Patches_path, "csv")
+	path_list, csv_paths = Utils.parse_dir(Train_folder, "csv")
+	
 	for csv_path in csv_paths:
-		wsi_name = str(csv_path).split("/")[-2]
-		with open(csv_path, "r") as prediction_file:
-			len_list = 0
-			preds = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0}
-			for line in islice(prediction_file, 1, None):
-				line_items = line.split("\t")
-				class_predicted = line_items[1]
-				confidence = line_items[2].replace("\n", "")
-				if confidence > "10":
-					preds[class_predicted] += 1
-				len_list +=1
-		
-		for i, classe in enumerate(preds):
 
-			with open(str(Config.args.Predictions) + "/" + wsi_name + ".csv", "a") as writer:
-				writer.write(f"classe : {classe} - pourcentage : {preds[classe]/len_list * 100}\n")
+		if str(csv_path).split("/")[-2] == 'Test_patches':
+			image_names_csv_file = csv_path
+		elif str(csv_path).split("/")[-2] == 'Predictions':
+			predictions_csv_file = csv_path
+
+	with open(image_names_csv_file, "r") as names:
+		with open(predictions_csv_file, "r") as preds:
+			for name_line in islice(names, 0, None):
+				classe = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
+				conf = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
+				wsi_name, patches_number = name_line.split("\t")
+				with open(str(output_folder) + "/" + wsi_name + ".csv", "w") as w:
+					writer = csv.writer(w, delimiter = '\t')
+					#writer.writerow(["name", "prediction", "confidence"])
+					for preds_line in islice(preds, 0, None):
+						patch_name, prediction, confidence = preds_line.split("\t")
+						confidence = confidence.replace("\n","")
+						if patch_name.split("_")[0] == wsi_name:
+							classe[prediction] += 1 
+							conf[prediction] += float(confidence)
+							#writer.writerow([patch_name, prediction, confidence])
+					#writer.writerow("\n")
+					sorted_classe = sorted(classe.items(), key = operator.itemgetter(1), reverse = True)
+					writer.writerow(["Classe", "Ratio", "Confidence"])
+					for sc in sorted_classe:
+						pourcentage = 100 * sc[1]/float(patches_number)
+						confidence_total = conf[sc[0]]/(sc[1] + 1e-7)
+						if confidence_total > 50:
+							writer.writerow([sc[0], pourcentage, confidence_total])
 
 if __name__ == '__main__':
 	get_prediction()
