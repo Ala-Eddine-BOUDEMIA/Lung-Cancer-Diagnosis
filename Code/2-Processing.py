4b123d46f1b6c93a1ac03810fa721f69654bc46a
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-06-03 09:44:17 +0100

    Data Augmentation

--- Code/2-Processing.py
+++ Code/2-Processing.py
@@ -1,100 +1,79 @@
 import Utils
 import Config
-import Code_from_deepslide
-##########################
-import os
-import csv
-import openslide
-import numpy as np
+#############
+import random
+from pathlib import Path
+########################
+import PIL
 from PIL import Image
-#####################
+from PIL import ImageFilter
+from PIL.ImageOps import flip, mirror
+########################################
 
-def generate_patches(Test_WSI, Train_WSI, Window_size, Test_Patches, Train_Patches, 
-	Validation_WSI, Validation_Patches, compression_factor):
+def transforms(key, image):
 
-	WSI_sets = [Train_WSI, Validation_WSI, Test_WSI]
-	output_folders = [Train_Patches, Validation_Patches, Test_Patches]
+	if key == "RandomNoise": 
+		transformed_image = image.filter(ImageFilter.GaussianBlur(1))
+	elif key == "RandomRotation": 
+		transformed_image = image.rotate(random.choice([90, 180, 270]))
+	elif key == "flip": 
+		transformed_image = flip(image)
+	elif key == "mirror": 
+		transformed_image = mirror(image)
 
-	for k in range(len(WSI_sets)):	
+	return transformed_image 
 
-		path_list, wsi_paths = Utils.parse_dir(input_directories = WSI_sets[k], forme = "svs")
-		Utils.create_folder(output_folders[k])
-		
-		with open(str(output_folders[k]) + "/" + "images names.csv", "w") as f:
-			writer = csv.writer(f, delimiter = "\t")
-			writer.writerow(["WSI name", "W", "H", "Number of generated patches", "increment_x", "increment_y"])
-			
-			for i in range(len(wsi_paths)): 
+def augmentor(patches, classes, maximum):
 
-				image_name = str(wsi_paths[i]).split("/")[-1][:-4] 
+	transformations = ["RandomNoise", "RandomRotation", "flip", "mirror"]
 
-				img = openslide.OpenSlide(str(wsi_paths[i]))
-				l = img.level_count
-				print("The number of the levels in the slide : ", l)
-				
-				w, h = img.level_dimensions[0] 
-				print("converting image.svs with width ", w," and height ", h)
-				if w % Window_size == 0:
-					increment_x = w // Window_size  
-				else:
-					increment_x = w // Window_size + 1
+	directories = sorted([d for d in patches.iterdir() if d.is_dir()])
 
-				if h % Window_size == 0:
-					increment_y = h // Window_size  
-				else:
-					increment_y = h // Window_size + 1
+	all_paths = {}
+	for directory in directories:
+		images_paths = sorted([f for f in directory.iterdir() if f.is_file()])
+		all_paths[str(directory).split("/")[-1]] = images_paths
 
-				i, j = 1, 0
-				for incre_x in range(increment_x):
-					for incre_y in range(increment_y): 
+	for subclass in all_paths.keys():
+		num_images_generated = 0
+		num_images_desired = maximum - len(all_paths[subclass])
+		paths = all_paths[subclass]
+		print(f"\nSubtype: {subclass}, \t Num images to be generated: {num_images_desired}")
+		print("Generating new images ...")
 
-						begin_x = incre_x * Window_size 
-						end_x = min(w, begin_x + Window_size) 
-						begin_y = incre_y * Window_size
-						end_y = min(h, begin_y + Window_size)
-						patch_w = end_x - begin_x
-						patch_h = end_y - begin_y
-						
-						if patch_w != Window_size:
-							begin_x = begin_x - (Window_size - patch_w) 
-							patch_w = end_x - begin_x
-						if patch_h != Window_size:
-							begin_y = begin_y - (Window_size - patch_h)
-							patch_h = end_y - begin_y
+		while num_images_generated < num_images_desired:
+			path = random.choice(paths)
+			image_name = str(path).split("/")[-1][:-5]
 
-						print("N°: ",i)
-						print('begin x: ', begin_x, " end x: ", end_x)
-						print('begin y: ', begin_y, " end y: ", end_y)
-						print("Size of the patch : ",patch_w," * ", patch_h)
+			rep = 0
+			for transform in transformations:
+				if transform in image_name.split("_"):
+					rep += 1
 
-						patch = img.read_region((begin_x, begin_y),0,(patch_w, patch_h))
-						
-						patch_rgb = Image.new("RGB", patch.size,(255,255,255))
-						patch_rgb.paste(patch) 
-						patch_rgb = patch_rgb.resize((int(patch_w/compression_factor), 
-									int(patch_h/compression_factor)), Image.ANTIALIAS) 
-						
-						patch_name = image_name + "_" + str(incre_x).zfill(4) + "_" + str(incre_y).zfill(4) + "_" + str(begin_x) + "_" + str(begin_y) +'.jpeg'
-						path = str(output_folders[k]) + "/" + patch_name
-						if k != 2 :
-							if Code_from_deepslide.is_purple(patch_rgb) == True :
-								patch_rgb.save(path) 
-								j += 1 
-						elif k == 2:
-							patch_rgb.save(path) 
-							j += 1
+			if rep == 0:
+				image_to_transform = Image.open(path)
+				num_transforms = 0
+				num_transforms_to_apply = random.randint(1, len(transformations))
+				transformed_image = PIL.Image.new("RGB", image_to_transform.size, (255,255,255))
+				transformed_image.paste(image_to_transform)
 
-						i += 1
+				keys = []
+				while num_transforms < num_transforms_to_apply:
+					key = random.choice(transformations)
+					while key not in keys:
+						keys.append(key)
+						transformed_image = transforms(key, transformed_image)
+						num_transforms += 1
 
-				writer.writerow([image_name, w, h, str(j), increment_x, increment_y])
+				new_path = '/'.join([str(patches), str(subclass), 
+							image_name + "_" + "_".join(sorted(keys))]) + ".tiff"
+
+				if Path(new_path).exists() == False:
+					transformed_image.save(new_path)
+					num_images_generated +=1
 
 if __name__ == '__main__':
-	generate_patches(
-	Test_WSI = Config.args.Test_WSI, 
-	Train_WSI = Config.args.Train_WSI, 
-	Window_size = Config.args.Window_size, 
-	Test_Patches = Config.args.Test_Patches,
-	Train_Patches = Config.args.Train_Patches, 
-	Validation_WSI = Config.args.Validation_WSI,
-	Validation_Patches = Config.args.Validation_Patches, 
-	compression_factor = Config.args.Compression_factor)
+	augmentor(
+	patches = Config.args.Patches,
+	classes = Config.args.Classes,
+	maximum = Config.args.Maximum)
