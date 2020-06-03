702e2011c27ab16906dea0b230466f53f33bd1cc
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-06-03 09:48:50 +0100

    Added confusion matrix and classification report

--- Code/Model_Utils.py
+++ Code/Model_Utils.py
@@ -1,22 +1,28 @@
 import Utils
-import Compute_mean_std
-import Code_from_deepslide
-##########################
+############
+import numpy as np
+import pandas as pd
+import seaborn as sns
+#####################
 import torch
 from torch import nn 
 from torchvision import models 
+import torch.nn.functional as F
 from torchsummary import summary
 from torchvision import datasets
 from torchvision import transforms                      
-################################
+##################################
 from matplotlib import pyplot as plt
-#################################### 
+####################################
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+#################################################
 
 def create_model():
                                                 
     model = models.resnet18(pretrained = True)  
     num_ftrs = model.fc.in_features             
-    model.fc = nn.Linear(num_ftrs, 6)              
+    model.fc = nn.Linear(num_ftrs, 5)              
     model_summary = summary(model, (3,224,224))
 
     return model
@@ -24,26 +30,21 @@ def create_model():
 def get_data_transforms(Train, path):
 
 	if Train:
-		mean, std = Compute_mean_std.compute_mean_std(path)
 		data_transforms = transforms.Compose(transforms = [ 
 			transforms.ColorJitter(brightness = 0.5, contrast = 0.5, saturation = 0.5, hue = 0.2),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            Code_from_deepslide.Random90Rotation(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean = mean, std = std)])
+            transforms.ToTensor()])
 	else :
-		mean, std = Compute_mean_std.compute_mean_std(path)
 		data_transforms = transforms.Compose(transforms = [ 
-			transforms.ToTensor(),
-            transforms.Normalize(mean = mean, std = std)])
+			transforms.ToTensor()])
 
 	return data_transforms
 
 def load_data(path, shuffle, batch_size, Train = True):
 
-	images_dataset = datasets.ImageFolder(root = str(path), transform = get_data_transforms(Train, path)) 
-	dataloaders = torch.utils.data.DataLoader(dataset = images_dataset, batch_size = batch_size, shuffle = shuffle, num_workers = 8)
+	images_dataset = datasets.ImageFolder(root = str(path), 
+										transform = get_data_transforms(Train, path)) 
+	dataloaders = torch.utils.data.DataLoader(dataset = images_dataset, batch_size = batch_size, 
+											shuffle = shuffle, num_workers = 8)
 
 	return dataloaders, images_dataset
 
@@ -54,24 +55,6 @@ def get_current_lr(opt):
 		current_lr = group["lr"]
 
 	return current_lr
-
-def plot_graphs(loss_history, metric_history, num_epochs):
-
-	plt.title("Train-Val Loss")
-	plt.plot(range(1, num_epochs + 1), loss_history['train'], label = 'train')
-	plt.plot(range(1, num_epochs + 1), loss_history['val'], label = 'val')
-	plt.ylabel("Loss")
-	plt.xlabel("Epochs")
-	plt.legend()
-	plt.show()
-
-	plt.title("Train-Val Accuracy")
-	plt.plot(range(1,num_epochs + 1), metric_history['train'], label = 'train')
-	plt.plot(range(1,num_epochs + 1), metric_history['val'], label = 'val')
-	plt.ylabel("Accuracy")
-	plt.xlabel("Epochs")
-	plt.legend()
-	plt.show()
 
 def save_work(epoch, save_interval, checkpoints_folder, model, opt, scheduler, val_metric):
 
@@ -85,3 +68,39 @@ def save_work(epoch, save_interval, checkpoints_folder, model, opt, scheduler, v
 			"optimizer_state_dict": opt.state_dict(),
 			"scheduler_state_dict": scheduler.state_dict(),
 			"epoch": epoch + 1}, f = str(output_path))
+
+def c_m(actual, predicted, classes):
+
+	remap_classes = {}
+	for i in range(len(classes)):
+		remap_classes[i] = classes[i]
+
+	actual = np.array(pd.Series(actual).replace(remap_classes))
+	predicted = np.array(pd.Series(predicted).replace(remap_classes))
+
+	cm = confusion_matrix(actual, predicted, labels = classes)
+
+	f1, ax1 = plt.subplots(1,1)
+	sns.heatmap(cm, annot = True, cmap = "YlGnBu")
+	ax1.set_xlabel('Predicted labels')
+	ax1.set_ylabel('True labels')
+	ax1.set_title('Confusion Matrix')
+
+	return f1, cm
+
+def c_r(actual, predicted, classes):
+
+	remap_classes = {}
+	for i in range(len(classes)):
+		remap_classes[i] = classes[i]
+
+	actual = np.array(pd.Series(actual).replace(remap_classes))
+	predicted = np.array(pd.Series(predicted).replace(remap_classes))
+	
+	cr = classification_report(actual, predicted, labels = classes, target_names = classes, output_dict = True)
+	cr = pd.DataFrame(cr)
+	f2, ax2 = plt.subplots(1,1)
+	ax2 = sns.heatmap(cr, annot = True, cmap = "YlGnBu")
+	ax2.set_title('Classification report')
+
+	return f2, cr
